933f87129c0599d15dc8a4e05827dc205c4767c3
Ellvon <pellinor99@hotmail.com>
2023-07-15 23:27:44 +0200

    Fixed comments

---
+++ EmailNotification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppDP
+{
+    //This method is being used to notify the observables when the ballanced is changed
+    public class EmailNotification : IObserver
+    {
+        public void Update(BankAccount account)
+        {
+            // Send an email to the user
+            Console.WriteLine("Email sent to user about balance change");
+        }
+    }
+}
---
+++ IObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAppDP
+{
+    //This declares the update method to be used in the observable pattern
+    public interface IObserver
+    {
+        void Update(BankAccount account);
+    }
+}
